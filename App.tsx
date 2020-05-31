c4c94900e3e8a72ac8dbae070640f1f13eb6a904
SuperJuice <wisconsinsara@gmail.com>
2020-05-30 20:04:30 -0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -3,17 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.view}>
+      <Text style={styles.text}>Hello, React Native!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  }
 });
