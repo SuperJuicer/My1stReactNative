c71f7408208f1d62badd3c666a05dbe89bfda0e3
SuperJuice <wisconsinsara@gmail.com>
2020-06-04 14:31:35 -0700

    Added a border. Discovered '1px solid black' is borderWidth: 1

--- App.tsx
+++ App.tsx
@@ -15,6 +15,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 2
   },
   text: {
     color: '#fff'
