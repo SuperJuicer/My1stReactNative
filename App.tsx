c2805088a59890670b8bd62d6c9e4307e5faa535
SuperJuice <wisconsinsara@gmail.com>
2020-06-09 15:15:50 -0700

    Added high contrast Text and View components.

--- App.tsx
+++ App.tsx
@@ -3,14 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.view}>
-      <Text style={styles.text}>Hello, React Native!</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.text}>Hello, React Native!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>This is my cool app.</Text>
+        <Text>It has Text components.</Text>
+        <Text>The text components have a high contrast color scheme.</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
     backgroundColor: '#000',
     alignItems: 'center',
@@ -20,6 +27,22 @@ const styles = StyleSheet.create({
     borderRadius: 2
   },
   text: {
-    color: '#fff'
+    color: '#000',
+    fontWeight: '800',
+    fontSize: 24
+  },
+  header: {
+    backgroundColor: '#ffc0cb',
+    padding: 20,
+    borderColor: '#000',
+    borderWidth: 1
+  },
+  body: {
+    backgroundColor: '#ff0',
+    padding: 20,
+    borderColor: '#000',
+    borderWidth: 1,
+    fontSize: 18,
+    fontWeight: '400'
   }
 });
